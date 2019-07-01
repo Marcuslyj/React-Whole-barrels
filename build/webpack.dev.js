fe04b64e2d89e2c97990872dc02e646b1e79a97c
liyongjie <liyongjie@tvflnet.com>
2019-07-01 11:40:02 +0800

    babel-loader

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,4 +1,4 @@
-const path  = require('path');
+const path = require('path');
 
 module.exports = {
     // 模式，表示dev环境
@@ -6,9 +6,17 @@ module.exports = {
     // 入口文件
     entry: '../src/index.js',
     // 让 webpack 能够去处理那些非 JavaScript 文件
-    module: {},
+    module: {
+        rules: [{
+            test: /.js$/,
+            exclude: /node_modules/, // 排除node_modules中的代码
+            use: [{
+                loader: 'babel-loader'
+            }],
+        }]
+    },
     // 插件
-    plugins:{},
+    plugins: {},
     output: {
         filename: 'bundle.js', // 打包后文件名称
         path: path.resolve(__dirname, '../dist')
